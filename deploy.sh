04f70c33dbffe6ce5acee92f95fffed01ae954c4
Daoyu Liu <daoyu@umu.com>
2025-02-27 14:17:45 +0800

    2025-02-27 14:17:41

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ function readDir(){
         if [ -d $1"/"$file ]; then
             readDir $1"/"$file
         else
-            scp -r $1"/"$file root@101.200.240.217:/alidata/www/default/$1"/"$file
+            scp -r $1"/"$file root@8.147.135.17:/usr/share/nginx/html/$1"/"$file
         fi
     done
 }
 
     
 # n # node 切到10.15
-hexo generate
+## hexo generate
 cd ./public
 rm -rf ./.DS_Store
 #readDir .
-scp -r ./ root@101.200.240.217:/alidata/www/default
+scp -r ./ root@8.147.135.17:/usr/share/nginx/html
 
 # n # node 切回来
