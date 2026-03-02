260ab08717f437fc312156c4630c35f3d0123ec3
daoyu <liudaoyu@outlook.com>
2026-03-02 15:17:51 +0800

    public

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ali-blog
 www.fleven.com
 2020-01-29 12:36:17
+
+链接
+ssh blog
+scp file.txt blog:/path/
--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,6 +1,25 @@
-# /bin/bash
+#!/bin/bash
+set -e
+
+# 1. 提交到 git
+git add .
+if ! git diff --staged --quiet; then
+  git commit -m "auto deploy: $(date '+%Y-%m-%d %H:%M')"
+  git push origin main
+fi
+
+# 2. 构建并发布博客
 npm run build
 cd ./public
 rm -rf ./.DS_Store
-scp -r ./* blog:/usr/share/nginx/html/
+
+# 使用 rsync 排除非动态生成/无需同步的文件
+rsync -avz --delete \
+  --exclude='.DS_Store' \
+  --exclude='Thumbs.db' \
+  --exclude='*.map' \
+  --exclude='*.log' \
+  --exclude='.git*' \
+  --exclude='*~' \
+  ./ blog:/usr/share/nginx/html/
 
