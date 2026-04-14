9fbcedca4bd3e3c860f026d6dd07525321977c22
daoyu <liudaoyu@outlook.com>
2026-04-15 01:29:56 +0800

    2026-04-14

--- themes/yilia/source-src/js/mobile.js
+++ themes/yilia/source-src/js/mobile.js
@@ -25,23 +25,23 @@ function tabActive() {
 	}
 }
 
-function getElementLeft(element) {　　　　
-	var actualLeft = element.offsetLeft;　　　　
-	var current = element.offsetParent;　　　　
-	while (current !== null) {　　　　　　
-		actualLeft += current.offsetLeft;　　　　
-		current = current.offsetParent;　　
-	}　　
+function getElementLeft(element) {
+	var actualLeft = element.offsetLeft;
+	var current = element.offsetParent;
+	while (current !== null) {
+		actualLeft += current.offsetLeft;
+		current = current.offsetParent;
+	}
 	return actualLeft;
-}　　
-function getElementTop(element) {　　　　
-	var actualTop = element.offsetTop;　　　　
-	var current = element.offsetParent;　　　　
-	while (current !== null) {　　　　　　
-		actualTop += current.offsetTop;　　　　　　
-		current = current.offsetParent;　　　　
-	}　　　　
-	return actualTop;　　
+}
+function getElementTop(element) {
+	var actualTop = element.offsetTop;
+	var current = element.offsetParent;
+	while (current !== null) {
+		actualTop += current.offsetTop;
+		current = current.offsetParent;
+	}
+	return actualTop;
 }
 
 function scrollStop($dom, top, limit, zIndex, diff) {
